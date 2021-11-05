85b458d4fbba3dce0434f0aba041e6dee101b119
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-11-05 15:15:16 +0100

    update rails app

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -1,12 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
-import $ from 'jquery';
-import 'select2';
-import 'select2/dist/css/select2.min.css';
+import "select2";
+import "select2/dist/css/select2.min.css";
 
 export default class extends Controller {
   connect() {
     $(this.element).select2({
-      width: '100%'
-    })
+      width: "100%"
+    });
   }
 }
--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,10 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
-import Typed from 'typed.js';
+import Typed from "typed.js";
 
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ['Drama', 'Classic', 'Comedy', 'Action', 'Romantic', 'Thriller', 'Horror', 'Slasher'],
+      strings: ["Drama", "Classic", "Comedy", "Action", "Romantic", "Thriller", "Horror", "Slasher"],
       typeSpeed: 40,
       loop: true,
       loopCount: Infinity,

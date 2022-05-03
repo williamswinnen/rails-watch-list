7644950506aa84e4e5aa4747cb089998875d7529
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-05-03 12:35:52 +0200

    lint

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
-import 'tom-select'
 import TomSelect from "tom-select"
 
 // Connects to data-controller="tom-select"
 export default class extends Controller {
   connect() {
-    new TomSelect(this.element, {});
+    new TomSelect(this.element, {})
   }
 }

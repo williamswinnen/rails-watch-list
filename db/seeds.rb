a3325d10e08c215e040b6fd8a8a82478bc9e664b
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-06-30 17:22:39 +0200

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-require 'open-uri'
-require 'json'
+require "open-uri"
+require "json"
 
 puts "Cleaning up database..."
 Movie.destroy_all
@@ -13,10 +13,10 @@ url = "http://tmdb.lewagon.com/movie/top_rated"
     puts "Creating #{movie['title']}"
     base_poster_url = "https://image.tmdb.org/t/p/original"
     Movie.create(
-      title: movie['title'],
-      overview: movie['overview'],
-      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
-      rating: movie['vote_average']
+      title: movie["title"],
+      overview: movie["overview"],
+      poster_url: "#{base_poster_url}#{movie["backdrop_path"]}",
+      rating: movie["vote_average"]
     )
   end
 end

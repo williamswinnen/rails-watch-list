8e64766db6f38f27f38be3c6d6e60d57680426ef
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-08-18 15:14:13 +0200

    fix tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists#index'
+  root to: "lists#index"
   resources :lists, except: [:edit, :update] do
-    resources :bookmarks, only: :create
+    resources :bookmarks, only: [:new, :create]
     resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy

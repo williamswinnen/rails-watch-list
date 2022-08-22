cfc59988db37353eb4f769f6d3f6e7adaf97909f
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-08-22 15:40:39 +0200

    improve solution

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
       redirect_to list_path(@list)
     else
       @bookmark = Bookmark.new
-      render 'lists/show', status: :unprocessable_entity
+      render "lists/show", status: :unprocessable_entity
     end
   end
 
